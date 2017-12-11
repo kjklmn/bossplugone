494deeb78a61808f44490a97886e99c0d913de71
kejian2275 <kejian@bdhongshan.com>
2017-12-11 14:16:39 +0800

    add butterknife

--- Hello/app/src/main/java/com/bdhs/bosspone/ui/activity/MainActivity.java
+++ Hello/app/src/main/java/com/bdhs/bosspone/ui/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.bdhs.bosspone.ui.activity;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,17 +22,29 @@ import com.bdhs.bosspone.ui.fragment.pond_fragment;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ViewPager View_Pager;
-    private LinearLayout home_layout_view;
-    private LinearLayout message_layout_view;
-    private LinearLayout pond_layout_view;
-    private LinearLayout person_layout_view;
-    private ImageView home_image_view;
-    private ImageView message_image_view;
-    private ImageView pond_image_view;
-    private ImageView person_image_view;
+    @BindView(R.id.View_Pager)
+    android.support.v4.view.ViewPager View_Pager;
+    @BindView(R.id.home_image_view)
+    ImageView home_image_view;
+    @BindView(R.id.home_layout_view)
+    LinearLayout home_layout_view;
+    @BindView(R.id.message_image_view)
+    ImageView message_image_view;
+    @BindView(R.id.message_layout_view)
+    LinearLayout message_layout_view;
+    @BindView(R.id.pond_image_view)
+    ImageView pond_image_view;
+    @BindView(R.id.pond_layout_view)
+    LinearLayout pond_layout_view;
+    @BindView(R.id.person_image_view)
+    ImageView person_image_view;
+    @BindView(R.id.person_layout_view)
+    LinearLayout person_layout_view;
 
     private FragmentPagerAdapter fragmentPagerAdapter;
     private List<Fragment> fragments;
@@ -37,23 +53,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setCustomActionBar();
+        initView();
+        initEvent();
+        SetSelect(0);   //首启动打开的界面为home_layout_view
+    }
 
-            initView();
-            initEvent();
-            SetSelect(0);   //首启动打开的界面为home_layout_view
+    private void setCustomActionBar() {
+        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
+        View mActionBarView = LayoutInflater.from(this).inflate(R.layout.actionbar_layout, null);
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar == null) {
+            Log.w("kejian", "actionBar is null");
+        }
+        actionBar.setCustomView(mActionBarView, lp);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 
     private void initView() {
-        View_Pager = (ViewPager) findViewById(R.id.View_Pager);
-        home_layout_view = (LinearLayout) findViewById(R.id.home_layout_view);
-        message_layout_view = (LinearLayout) findViewById(R.id.message_layout_view);
-        pond_layout_view = (LinearLayout) findViewById(R.id.pond_layout_view);
-        person_layout_view = (LinearLayout) findViewById(R.id.person_layout_view);
-        home_image_view = (ImageView) findViewById(R.id.home_image_view);
-        message_image_view = (ImageView) findViewById(R.id.message_image_view);
-        pond_image_view = (ImageView) findViewById(R.id.pond_image_view);
-        person_image_view = (ImageView) findViewById(R.id.person_image_view);
-
         //新建滑动的4个数据源 fragments数据集中
         fragments = new ArrayList<>();
         Fragment home = new home_fragment();
@@ -81,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         View_Pager.setAdapter(fragmentPagerAdapter);
 
         //View_Pager和itemMenu进行联动效果
-        View_Pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        View_Pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -90,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onPageSelected(int position) {
                 ReSetimg();
-               int Currentitem = View_Pager.getCurrentItem();
+                int Currentitem = View_Pager.getCurrentItem();
                 /**
                  *  文件进行索引调用 (Alt+Shift+m)
                  */
@@ -103,6 +124,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
     //添加菜单点击事件
     private void initEvent() {
         home_layout_view.setOnClickListener(this);
@@ -110,21 +132,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pond_layout_view.setOnClickListener(this);
         person_layout_view.setOnClickListener(this);
     }
+
     //点击后的相关操作
     @Override
     public void onClick(View v) {
         ReSetimg();
         switch (v.getId()) {
-            case R.id.home_layout_view :
+            case R.id.home_layout_view:
                 SetSelect(0);
                 break;
-            case R.id.message_layout_view :
+            case R.id.message_layout_view:
                 SetSelect(1);
                 break;
-            case R.id.pond_layout_view :
+            case R.id.pond_layout_view:
                 SetSelect(2);
                 break;
-            case R.id.person_layout_view :
+            case R.id.person_layout_view:
                 SetSelect(3);
                 break;
             default:
@@ -140,26 +163,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //设置点击菜单后当前图片变亮,同时View_Pager滑动到指定的界面
     private void Switch_Tab(int a) {
         switch (a) {
-            case 0 :
+            case 0:
                 View_Pager.setCurrentItem(0);
                 home_image_view.setImageResource(R.drawable.comui_tab_home_selected);
                 break;
-            case 1 :
+            case 1:
                 View_Pager.setCurrentItem(1);
                 message_image_view.setImageResource(R.drawable.comui_tab_message_selected);
                 break;
-            case 2 :
+            case 2:
                 View_Pager.setCurrentItem(2);
                 pond_image_view.setImageResource(R.drawable.comui_tab_pond_selected);
                 break;
-            case 3 :
+            case 3:
                 View_Pager.setCurrentItem(3);
                 person_image_view.setImageResource(R.drawable.comui_tab_person_selected);
                 break;
-                default:
-                    break;
+            default:
+                break;
         }
     }
+
     //设置所有图片的颜色为暗色
     private void ReSetimg() {
         home_image_view.setImageResource(R.drawable.comui_tab_home);
--- Hello/app/src/main/java/com/bdhs/bosspone/ui/fragment/home_fragment.java
+++ Hello/app/src/main/java/com/bdhs/bosspone/ui/fragment/home_fragment.java
@@ -3,6 +3,7 @@ package com.bdhs.bosspone.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,25 @@ import com.bdhs.bosspone.R;
 public class home_fragment extends Fragment {
 
     private View ContentView;
+    private SwipeRefreshLayout mRefreshLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        ContentView = inflater.inflate(R.layout.home_layout_view,container,false);
+        ContentView = inflater.inflate(R.layout.fragment_fpage,container,false);
+        mRefreshLayout = (SwipeRefreshLayout) ContentView.findViewById(R.id.main_refresh_layout);
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRefreshLayout.setRefreshing(false);
+                    }
+                }, 500);
+            }
+        });
         return ContentView;
     }
 }
